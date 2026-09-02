b9f4a76bae4e87c1701b0fa5e4f40756895a1d38
Ideogram Manager <user@example.com>
2026-09-02 16:31:46 +0200

    update.js: add git pull step

--- update.js
+++ update.js
@@ -3,6 +3,12 @@ module.exports = {
     {
       method: "shell.run",
       params: {
+        message: ["git pull"]
+      }
+    },
+    {
+      method: "shell.run",
+      params: {
         path: "app",
         message: ["npm i --no-audit --no-fund --quiet --omit=dev"]
       }
